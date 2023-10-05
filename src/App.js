b2aad05c2a923a6d82f51fe0c90486a4156c3d45
Max-Black-De <black_de@yahoo.com>
2023-10-05 16:28:52 +0500

    Created new react prjct

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Привет, Максимка. Корректируй <code>src/App.js</code> и сохрани чтобы перезагрузить.
         </p>
         <a
           className="App-link"
